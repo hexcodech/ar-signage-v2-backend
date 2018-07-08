4f05cd3b781b1989961ed5aa315f0d8c8d386ade
Marc Berchtold <marc.berchtold@hotmail.de>
2018-07-08 18:43:55 +0200

    cors, fix pattern matching, clients/rooms url for dashboard

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Express = require('express');
 const Mqtt = require('mqtt');
 const Jsonfile = require('jsonfile');
+const Cors = require('cors');
 const clientsJsonFilePath = './clients.json';
 const roomsJsonFilePath = './rooms.json';
 const config = require('./config.json');
@@ -39,13 +40,18 @@ module.exports = class App {
 
     routes() {
         console.log(`--- Registering routes & middlewares`);
+        this.express.use(Cors());
         this.express.use('/mediaCache', Express.static(__dirname + '/media'));
+        this.express.use('/rooms', Express.static(__dirname + '/rooms.json'));
+        this.express.use('/clients', Express.static(__dirname + '/clients.json'));
     }
 
     mqttEvents() {
         this.mqtt.on('connect', () => {
             console.log(`--- Connected to mqtt server! Subscribing to topics`);
             this.mqtt.subscribe(`ar-signage/devicediscovery`);
+            this.mqtt.subscribe(`ar-signage/+/timer/setseconds`);
+            this.mqtt.subscribe(`ar-signage/+/timer/control`);
         });
 
         this.mqtt.on('error', () => {
@@ -58,26 +64,29 @@ module.exports = class App {
 
     mqttMessageHandler(topic, message) {
         let messageObject;
+        let roomname;
         try {
             messageObject = JSON.parse(message.toString());
         } catch (err) {
             console.error(`mqttMessageHandler JSON parse error: ${err.toString()}`);
         }
-
+        console.log(topic);
+        console.dir(messageObject);
+        console.log();
         // Abusing switch for regex matching... This is sooo evil and genius I love it :P
         switch (true) {
             case topic === 'ar-signage/devicediscovery':
-                if (messageObject.value.uuid) {
+                if (messageObject.value.uuid && messageObject.value.role === 'client') {
                     this.mqtt.publish(`ar-signage/client/${messageObject.value.uuid}/mediacacheurl`, JSON.stringify({
-                        value: `${config.bindingIP}:${config.bindingPort}/mediaCache`
-                    }), {retain: true});
+                        value: `http://${config.bindingIP}:${config.bindingPort}/mediaCache`
+                    }));
 
                     Jsonfile.readFile(clientsJsonFilePath, (err, obj) => {
                         if (obj[messageObject.value.uuid] && obj[messageObject.value.uuid].roomname && obj[messageObject.value.uuid].clientname) {
                             console.log(`- Defined client ${obj[messageObject.value.uuid].clientname} (${messageObject.value.uuid}) discovered!`);
                             this.mqtt.publish(`ar-signage/client/${messageObject.value.uuid}/roomname`, JSON.stringify({
                                 value: obj[messageObject.value.uuid].roomname
-                            }), {retain: true});
+                            }));
                         } else {
                             console.log(`- Undefined client ${messageObject.value.uuid} discovered! Adding template entry to clients.json`);
                             obj[messageObject.value.uuid] = {};
@@ -88,19 +97,29 @@ module.exports = class App {
                             });
                             this.mqtt.publish(`ar-signage/client/${messageObject.value.uuid}/roomname`, JSON.stringify({
                                 value: obj[messageObject.value.uuid].roomname
-                            }), {retain: true});
+                            }));
                         }
                     });
+                } else if (messageObject.value.role === 'dashboard') {
+                    this.mqtt.publish(`ar-signage/dashboard/mediacacheurl`, JSON.stringify({
+                        value: `http://${config.bindingIP}:${config.bindingPort}/mediaCache`
+                    }));
+                    this.mqtt.publish(`ar-signage/dashboard/roomsurl`, JSON.stringify({
+                        value: `http://${config.bindingIP}:${config.bindingPort}/rooms`
+                    }));
+                    this.mqtt.publish(`ar-signage/dashboard/clientsurl`, JSON.stringify({
+                        value: `http://${config.bindingIP}:${config.bindingPort}/clients`
+                    }));
                 }
                 break;
-            case topic.match(/^ar-signage\/.+\/timer\/setseconds$/g):
-                const roomname = topic.split(/[\/\/]/g)[1];
+            case topic.match(/^ar-signage\/.+\/timer\/setseconds$/g) && topic.match(/^ar-signage\/.+\/timer\/setseconds$/g).length > 0:
+                roomname = topic.split(/[\/\/]/g)[1];
                 const value = parseInt(messageObject.value, 10);
                 this.timers[roomname].originalSeconds = value;
                 this.timers[roomname].seconds = value;
                 break;
-            case topic.match(/^ar-signage\/.+\/timer\/control$/g):
-                const roomname = topic.split(/[\/\/]/g)[1];
+            case topic.match(/^ar-signage\/.+\/timer\/control$/g) && topic.match(/^ar-signage\/.+\/timer\/control$/g).length > 0:
+                roomname = topic.split(/[\/\/]/g)[1];
                 switch (messageObject.value) {
                     case 'START':
                         if (this.timers[roomname].seconds > 0) {
